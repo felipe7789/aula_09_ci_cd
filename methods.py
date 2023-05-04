a6c886df9997922a75f0d540fbdfcf297ccfe75a
felipe7789 <oshirokfelipe@gmail.com>
2023-05-04 18:15:29 -0300

    descrição breve do commit

--- methods.py
+++ methods.py
@@ -5,3 +5,15 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def soma(x,y):
+    return x+y
+
+def subtr(x,y):
+    return x-y
+
+def multi(x,y):
+    return x*y
+
+def divi(x,y):
+    return x/y
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,34 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def test_soma():
+    num1=3
+    num2=4
+    output=methods.soma(num1,num2)
+    assert output==7
+
+def test_multi():
+    num3=2
+    num4=2
+    output=methods.multi(num3,num4)
+    assert output==4
+
+def test_subtr():
+    num5=4
+    num6=2
+    output=methods.subtr(num5,num6)
+    assert output==2
+
+def test_divi():
+    num7=8
+    num8=4
+    output=methods.divi(num7,num8)
+    assert output==2
+
+test_area()
+test_perimeter()
+test_soma()
+test_multi()
+test_subtr()
+test_divi()
